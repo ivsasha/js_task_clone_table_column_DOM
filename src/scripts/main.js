aaa7c2fc1cc21565b91916ccaeb8d0757d066179
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-23 20:31:41 +0300

    clone column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const sections = [table.tHead, ...table.tBodies, table.tFoot];
+
+sections.forEach((section) => {
+  for (const row of section.rows) {
+    const firstCell = row.cells[1];
+
+    const clone = firstCell.cloneNode(true);
+
+    row.insertBefore(clone, row.cells[row.cells.length - 1]);
+  }
+});
