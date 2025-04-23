2fec4c92db84884dacbad98cd1c31f238295fbc8
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-23 20:35:40 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,18 @@ const sections = [table.tHead, ...table.tBodies, table.tFoot];
 
 sections.forEach((section) => {
   for (const row of section.rows) {
+    if (row.cells.length < 2) {
+      continue;
+    }
+
     const firstCell = row.cells[1];
 
     const clone = firstCell.cloneNode(true);
 
-    row.insertBefore(clone, row.cells[row.cells.length - 1]);
+    if (row.cells.length > 2) {
+      const lastCell = row.cells[row.cells.length - 1];
+
+      lastCell.parentNode.insertBefore(clone, lastCell);
+    }
   }
 });
